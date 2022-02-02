25f23d63e7eb719cf966d1c792e093d54f90108f
adityaShekhar <aditya.s@toppr.com>
2022-02-02 23:09:24 +0530

    added Facebook case

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import InstaDownload from "./components/InstaDownload";
 function App() {
   return (
     // need to add STate Management Library
+    // Handle Facebook URLs : A component
     <div className="App">
       <InstaDownload />
     </div>
